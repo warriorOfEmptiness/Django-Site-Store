3b318f8abe87fffc153eecbe8350a737a93022ac
warriorOfEmptiness <phDiscordAndreyka@yandex.ru>
2025-07-06 18:56:05 +0300

    add models
    написанны модели в приложение store
    
    написал модели ProductCategory и product

--- backend/store/models.py
+++ backend/store/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=35)
+    dsc = models.TextField(max_length=456,blank=True,null=True)
+
+    def __str__(self):
+        return self.name 
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=156,unique=True)
+    dsc = models.TextField(max_length=425)
+    price = models.DecimalField(decimal_places=1,max_digits=7)
+    image = models.ImageField(upload_to='catalog_images')
+
+    def __str__(self):
+        return f'{self.name} | {self.price}'
+    
+
+
+    
